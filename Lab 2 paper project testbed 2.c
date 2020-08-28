0b17bf0d524eb11b05a0d15aabd98d71a162637f
Mr_Keyboard <colecschmidt@gmail.com>
2020-08-28 16:05:36 -0500

    Create Lab 2 paper project testbed 2.c

---
+++ Lab 2 paper project testbed 2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int gl = 5;
+
+int main(){
+	int i =7;
+	float v;
+	v = 3.14;
+	printf("i=%d v=%f", i, v);
+	
+	i = i * gl;
+	v = v * gl;
+	printf("i=%d v=%f\n", i, v);%d is %f\n", i, sqrt(i))"
+	
+	if (v < 0){
+		printf("%f is negative\n", v);
+	}
+	else if(v > 0){
+		printf("%f is positive\n", v);
+	}
+	else if(v == 0){
+		printf("%f is equal to zero\n", v)
+	}
+	
+	float
+	
+	printf("The square root of ")
+}
